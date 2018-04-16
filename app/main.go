c4777a46cfa232162e1ca7e44e05c8b7b7074d8e
Rahul Yadav <rahul.yadav@hotcocoasoftware.com>
2018-04-16 18:08:44 +0530

    Couchbase done.

--- app/main.go
+++ app/main.go
@@ -8,6 +8,7 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/couchbase/go-couchbase"
 	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis"
 	"github.com/olivere/elastic"
@@ -52,6 +53,71 @@ func errorResponse(c *gin.Context, code int, err string) {
 	c.JSON(code, gin.H{
 		"error": err,
 	})
+}
+
+func couchGet(c *gin.Context) {
+	query := c.Query("query")
+	if query == "" {
+		errorResponse(c, http.StatusBadRequest, "Query not specified")
+		return
+	}
+	cl, err := couchbase.Connect("http://couchbase-master-service:8091")
+	if err != nil {
+		log.Fatalf("Error connecting:  %v", err)
+	}
+
+	pool, err := cl.GetPool("default")
+	if err != nil {
+		errorResponse(c, http.StatusInternalServerError, "cannot get pool")
+		return
+	}
+
+	bucket, err := pool.GetBucket("default")
+	if err != nil {
+		errorResponse(c, http.StatusInternalServerError, "cannot get bucket")
+		return
+	}
+	var values interface{}
+	err = bucket.Get(query, &values)
+	if err != nil {
+		errorResponse(c, http.StatusInternalServerError, "cannot insert into couchbase")
+		return
+	}
+	c.JSON(http.StatusOK, values)
+}
+
+func couchInsert(c *gin.Context) {
+	type request struct {
+		Key    string
+		Values []string
+	}
+	var postParams request
+	if err := c.BindJSON(&postParams); err != nil {
+		errorResponse(c, http.StatusBadRequest, "Malformed request body")
+		return
+	}
+	cl, err := couchbase.Connect("http://couchbase-master-service:8091")
+	if err != nil {
+		log.Fatalf("Error connecting:  %v", err)
+	}
+
+	pool, err := cl.GetPool("default")
+	if err != nil {
+		errorResponse(c, http.StatusInternalServerError, "cannot get pool")
+		return
+	}
+
+	bucket, err := pool.GetBucket("default")
+	if err != nil {
+		errorResponse(c, http.StatusInternalServerError, "cannot get bucket")
+		return
+	}
+
+	err = bucket.Set(postParams.Key, 0, postParams.Values)
+	if err != nil {
+		errorResponse(c, http.StatusInternalServerError, "cannot insert into couchbase")
+		return
+	}
 }
 
 func createDocumentsEndpoint(c *gin.Context) {
@@ -151,22 +217,34 @@ func searchEndpoint(c *gin.Context) {
 
 func main() {
 	var err error
-	//for {
 	elasticClient, err = elastic.NewClient(
 		elastic.SetURL("http://elasticsearch:9200"),
 		elastic.SetSniff(false),
 	)
-	//	if err != nil {
-	//		log.Println(err)
-	//		time.Sleep(3 * time.Second)
-	//	} else {
-	//		break
-	//	}
-	//}
+	if err != nil {
+		log.Println(err)
+	}
+	go func() {
+		time.Sleep(3 * time.Second)
+		for {
+			elasticClient, err = elastic.NewClient(
+				elastic.SetURL("http://elasticsearch:9200"),
+				elastic.SetSniff(false),
+			)
+			if err != nil {
+				log.Println(err)
+				time.Sleep(3 * time.Second)
+			} else {
+				break
+			}
+		}
+	}()
 	r := gin.Default()
 	r.POST("/documents", createDocumentsEndpoint)
 	r.GET("/search", searchEndpoint)
 	r.GET("/redis", redisH)
+	r.POST("/couchbaseInsert", couchInsert)
+	r.GET("/couchbase", couchGet)
 	r.GET("/", handler)
 	if err = r.Run(":8080"); err != nil {
 		log.Fatal(err)
